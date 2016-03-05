fa4b8c93faa80c9405e097af3c190528f229bc72
Nathaniel-Pankhurst <Nathaniel.Pankhurst@outlook.com>
2016-03-05 16:28:16 +0000

    Code completed, and validation implemented
    the program is fully working and adequate validation has been
    implemented, aside from in the places pointed out in the README

--- BARcharts.py
+++ BARcharts.py
@@ -1,12 +1,12 @@
 # Binary Tree traversal software
 #
 #Author: Nathaniel Pankhurst
-#Date: 04/03/2016
-
-#Tree defined  for testing purposes only
-tree = [(1, "Mr Big", 0), (2, "Alan Amis", 1), (3, "Bob Bridger", 1), (6, "Charlie Chubb", 2), (12, "Dave Dell", 3), (15, "Ernie East", 3), (16, "Fred Fish", 6), (17, "George Green", 6)]
+#Date started: 04/03/2016
+#Date finished: 05/03/2016
 
 def getUserChoice():
+# Function called in the main() function to give user the range of choices, pull the user's choice as an input,
+# then validate that choice.
     inputValid = False
     while not inputValid:
         userChoice = input("Please choose from one of the options Below: \n     1: Build Org Chart \n     2: Find shortest path through chain of management between 2 employees \n     Q: Quit the Program \nuserChoice: ")
@@ -20,17 +20,33 @@ def getUserChoice():
     return userChoice
 
 def getTree(tree):
+# Function called as the first option from the main() function used to take user inputs and then build these inputs into a list
+# of tuples that represents the organisational hierarchy chart
+# All inputs in this function have been validated to make sure that they fit into their respective intended type,
+# and in the case of employee number to ensure that no employee number is repeated.
     treeFinished = False
+    repetitionCount = 1
     while not treeFinished:
         numberValid = False
         nameValid = False
         managerValid = False
-        repetitionCount = 1
+        checkPresent = False
+        
         while not numberValid:
-             try: 
+             try:
                 employeeNumber = int(input("What is the employee number for employee " + str(repetitionCount)+ "?: "))
-                int(employeeNumber)
-                numberValid = True
+                if employeeNumber >= 1:
+                    if repetitionCount > 1:
+                        for j in range(repetitionCount - 1):
+                            if employeeNumber == tree[j][0]:
+                                checkPresent = True
+                    if not checkPresent:
+                        numberValid = True
+                    else:
+                        print("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\nError: There is already an employee with that number, please try again.\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n")
+                else:
+                    print("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\nError: Please enter a value greater than 0 for the employee number.\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n")
+                    input("*Press Enter to continue")
              except ValueError:
                 print("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\nError: user input invalid, please try again.\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n")
                 input("*Press Enter to continue*")
@@ -45,9 +61,12 @@ def getTree(tree):
                 nameValid = False
         while not managerValid:
             try:
-                employeeManager = input("What is the employee number of employee " + str(repetitionCount) + "'s manager? (enter 0 if employee has no manager): ")
-                int(employeeManager)
-                managerValid = True
+                employeeManager = int(input("What is the employee number of employee " + str(repetitionCount) + "'s manager? (enter 0 if employee has no manager): "))
+                if employeeManager >= 0:     
+                    managerValid = True
+                else:
+                    print("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\nError: Please enter a value greater than or equal to 0 for the employee's manager number.\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n")
+                    input("*Press Enter to continue")
             except ValueError:
                 print("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\nError: user input invalid, please try again.\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n")
                 input("*Press Enter to continue*")
@@ -60,6 +79,8 @@ def getTree(tree):
 
 
 def checkFinished():
+# Function implemented by getTree() that checks whether or not the user has finished entering in the values for the tree,
+# and then returns a boolean value stating the result.
     repeat = False
     valid = False
     while not valid:
@@ -77,7 +98,28 @@ def checkFinished():
             input("*Press Enter to continue*")
     return repeat
 
+def traverseTree():
+# Function called by main() for the purpose of finding, and building the shortest path of communication between two employees.
+# For this to occur traverseTree pulls the employeeId's of the user's chosen employees through use of getEmployeeId(),
+# passes those into getEmployeeChainOfCommand() to find the chains of command for both employee,
+# and then finally prints out to the user the shortest path between the two employees that was found by
+# passing in both chains of command into findShortestPath()
+
+    senderCoC = [""]
+    recipientCoC = [""]
+    sender = getEmployeeId("Start", tree)
+    recipient = getEmployeeId("End", tree)
+    senderCoC = getEmployeeChainOfCommand(sender)
+    recipientCoC = getEmployeeChainOfCommand(recipient)
+    shortestPath = findShortestPath(senderCoC, recipientCoC)
+    print("\nThe shortest chain of communication between the two employees is: " + shortestPath + "\n")
+    input("*Press Enter to continue*")
+
 def getEmployeeId(nameType, tree):
+# Function used by traverseTree() to find the employee number of an employee in the org chart
+# who has a name that is obtained through user input.
+# Validation in place to check that the inputted name is present in the Org Chart.
+
     nameValid = False
     nameFound = False
     while not nameValid:
@@ -92,21 +134,13 @@ def getEmployeeId(nameType, tree):
             elif not nameFound:
                 if i == (len(tree) - 1):
                     nameValid = False
-                    print("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\nError: This name isn't present in the Organisational chart, please enter another name.\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+                    print("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\nError: This name isn't present in the Organisational chart, please enter another name.\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
                     input("*Press Enter to continue*")
     return employeeId
 
-def traverseTree():
-    senderCoC = [""]
-    recipientCoC = [""]
-    sender = getEmployeeId("Start", tree)
-    recipient = getEmployeeId("End", tree)
-    senderCoC = getEmployeeChainOfCommand(sender)
-    recipientCoC = getEmployeeChainOfCommand(recipient)
-    shortestPath = findShortestPath(senderCoC, recipientCoC)
-    print("The shortest chain of communication between the two employees is: " + shortestPath)
     
 def getEmployeeChainOfCommand(employeeId):
+# Function used by traverseTree() to find the chain of command for a given employee.
     chainOfCommand = []
     toBeSearched = employeeId
     listComplete = False
@@ -120,26 +154,29 @@ def getEmployeeChainOfCommand(employeeId):
     return chainOfCommand
 
 def findShortestPath(senderCoC, recipientCoC):
+# Function used by traverseTree() that compares two different chains of command to find the shortest route of communication
+# between two employees, returning this route as a string.
     shortestPath = ""
     intersectionFound = False
     for i in range(len(senderCoC)):
-
         if not intersectionFound:
             if not (i == 0):
                 shortestPath = shortestPath + " -> "
             shortestPath = shortestPath + senderCoC[i]
         for j in range(len(recipientCoC)):
             if senderCoC[i] == recipientCoC[j]:
-                intersectionFound = True
-                backStep = j - 1
-                while not backStep == -1:
-                    shortestPath = shortestPath + " <- " + recipientCoC[backStep]
-                    backStep = backStep - 1
+                if not intersectionFound:
+                    intersectionFound = True
+                    backStep = j - 1
+                    while not backStep == -1:
+                        shortestPath = shortestPath + " <- " + recipientCoC[backStep]
+                        backStep = backStep - 1
     return shortestPath
                 
             
 def main():
-#The main function, called upon launch of the program.
+# This is the main function, upon calling the program this calls the functions used to determine the user's choice of action
+# then routes the user to the functins needed to complete the intended task.
         print("---------------------------------")
         print("------------BARCharts------------")
         print("---------------------------------")
