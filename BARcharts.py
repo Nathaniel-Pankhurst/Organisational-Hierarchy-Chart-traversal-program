3a935bd75b6ea6c55729a0efff152ec217bdb896
Nathaniel-Pankhurst <Nathaniel.Pankhurst@outlook.com>
2016-03-05 02:49:39 +0000

    Functionality of code complete
    The core functionality of the code is complete, providing correct
    outputs with the right inputs. Just need to vigorously test to ensure
    watertight validation is in place, and make sure all functions are
    adequately documented.

--- BARcharts.py
+++ BARcharts.py
@@ -3,22 +3,15 @@
 #Author: Nathaniel Pankhurst
 #Date: 04/03/2016
 
-#All use of threading was done with the help of: http://www.tutorialspoint.com/python/python_multithreading.htm
-import threading
+#Tree defined  for testing purposes only
+tree = [(1, "Mr Big", 0), (2, "Alan Amis", 1), (3, "Bob Bridger", 1), (6, "Charlie Chubb", 2), (12, "Dave Dell", 3), (15, "Ernie East", 3), (16, "Fred Fish", 6), (17, "George Green", 6)]
 
-class searchThread(threading.Thread):
-    def _init_(self, threadID, searchName):
-        threading.Thread._init_(self)
-        self.threadID = threadID
-        self.searchName = searchName
-    def run(self):
-        print(self.searchName)
-    
 def getUserChoice():
     inputValid = False
     while not inputValid:
-        userChoice = input("Please choose from one of the options Below: \n     1: Build Org Chart \n     2: Find path of management between 2 employees \nuserChoice: ")
-        if(userChoice == "1") or (userChoice == "2"):
+        userChoice = input("Please choose from one of the options Below: \n     1: Build Org Chart \n     2: Find shortest path through chain of management between 2 employees \n     Q: Quit the Program \nuserChoice: ")
+        userChoice = userChoice.lower()
+        if(userChoice == "1") or (userChoice == "2") or (userChoice == "q"):
             inputValid = True
         else:
             inputValid = False
@@ -84,7 +77,7 @@ def checkFinished():
             input("*Press Enter to continue*")
     return repeat
 
-def getName(nameType, tree):
+def getEmployeeId(nameType, tree):
     nameValid = False
     nameFound = False
     while not nameValid:
@@ -94,29 +87,57 @@ def getName(nameType, tree):
             if name == tree[i][1]:
                 nameValid = True
                 nameFound = True
+                employeeId = tree[i][0]
                 print("Employee " + name + " found.")
             elif not nameFound:
                 if i == (len(tree) - 1):
                     nameValid = False
                     print("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\nError: This name isn't present in the Organisational chart, please enter another name.\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
                     input("*Press Enter to continue*")
-    return name
+    return employeeId
 
 def traverseTree():
-    #Tree defined  for testing purposes only
-    tree = [(1, "Mr Big", 0), (2, "Alan Amis", 1), (3, "Bob Bridger", 1), (6, "Charlie Chubb", 2), (12, "Dave Dell", 3), (15, "Ernie East", 3), (16, "Fred Fish", 6), (17, "George Green", 6)]
-    fromStartQueue = []
-    fromEndStack = []
-    name1 = getName("Start", tree)
-    name2 = getName("End", tree)
-    searchStart = searchThread(1, name1)
-    searchEnd = searchThread(2, name2)
-    searchStart.start()
-    searchEnd.start()
-
-def threadTest():
-    tree = [(1, "Mr Big", 0), (2, "Alan Amis", 1), (3, "Bob Bridger", 1), (6, "Charlie Chubb", 2), (12, "Dave Dell", 3), (15, "Ernie East", 3), (16, "Fred Fish", 6), (17, "George Green", 6)]
+    senderCoC = [""]
+    recipientCoC = [""]
+    sender = getEmployeeId("Start", tree)
+    recipient = getEmployeeId("End", tree)
+    senderCoC = getEmployeeChainOfCommand(sender)
+    recipientCoC = getEmployeeChainOfCommand(recipient)
+    shortestPath = findShortestPath(senderCoC, recipientCoC)
+    print("The shortest chain of communication between the two employees is: " + shortestPath)
     
+def getEmployeeChainOfCommand(employeeId):
+    chainOfCommand = []
+    toBeSearched = employeeId
+    listComplete = False
+    while not listComplete:
+        for i in range(len(tree)):
+            if toBeSearched == tree[i][0]:
+                chainOfCommand.append(tree[i][1])
+                toBeSearched = tree[i][2]
+            elif toBeSearched == 0:
+                listComplete = True
+    return chainOfCommand
+
+def findShortestPath(senderCoC, recipientCoC):
+    shortestPath = ""
+    intersectionFound = False
+    for i in range(len(senderCoC)):
+
+        if not intersectionFound:
+            if not (i == 0):
+                shortestPath = shortestPath + " -> "
+            shortestPath = shortestPath + senderCoC[i]
+        for j in range(len(recipientCoC)):
+            if senderCoC[i] == recipientCoC[j]:
+                intersectionFound = True
+                backStep = j - 1
+                while not backStep == -1:
+                    shortestPath = shortestPath + " <- " + recipientCoC[backStep]
+                    backStep = backStep - 1
+    return shortestPath
+                
+            
 def main():
 #The main function, called upon launch of the program.
         print("---------------------------------")
@@ -131,8 +152,11 @@ def main():
                 getTree(tree)
             elif (userChoice == "2"):
                 traverseTree()
+            elif (userChoice == "q"):
+                endMain = True
             else:
                 print("You broke something...")
-            endMain = True
                     
-#main()
+main()
+quit()
+
